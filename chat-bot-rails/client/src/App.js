ee1127c29e0795bce544a8c2b67dd0db6d5aa5a9
joesephorekhov <josephorekhov@gmail.com>
2021-10-26 12:48:09 -0600

    dev

--- chat-bot-rails/client/src/App.js
+++ chat-bot-rails/client/src/App.js
@@ -1,17 +1,41 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const userInput = "cow go boy girl sit"
 
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
+  // const [ wordsArr, setWordArr] = useState([])
+  let newArr = []
 
-  return (
+  // const arr = userInput.split(' ')
+  // arr.forEach(word => wordSearch(word))
+
+  // function wordSearch(word){
+  //     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+  //     .then(resp=> resp.json())
+  //     .then(data =>{
+  //       wordConverter(data)
+  //       // setWordArr([...wordsArr, data[0].meanings[0].partOfSpeech])
+  //   })
+  // }
+
+  function wordConverter(word){
+    newArr.push(word[0].meanings[0].partOfSpeech)
+  }
+  console.log(newArr)
+
+  function respond(word){
+    if( word === "sad" || word === "bad" || word === "depressed" ){
+      console.log( `Sorry to hear that you're ${word},`)
+    }else if( word === "happy" ){
+      console.log ("Fantastic! What has cuased you to be happy?")
+    }else 
+      console.log( "Could you explain?")
+  };
+
+respond('depressed')
+  return(
     <div className="App">
-      <h1>Page Count: {count}</h1>
+
     </div>
   );
 }
